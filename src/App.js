8b3915c1bc4f504c5ccc99dcb374655b3004e6a1
AugustoTI <augustocesarti2021@gmail.com>
2022-01-14 13:05:03 -0300

    criando meu proprio Hook

--- src/App.js
+++ src/App.js
@@ -1,62 +1,37 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
-import P from 'prop-types';
-import { useReducer, createContext, useContext, useRef } from 'react';
 
-// data.js
-export const globalState = {
-  title: 'O titulo do contexto',
-  body: 'O corpo do contexto',
-  counter: 0,
+const useMyHook = (callback, delay = 1000) => {
+  const savedCB = useRef();
+
+  useEffect(() => {
+    savedCB.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      savedCB.current();
+    }, delay);
+
+    return () => clearInterval(interval);
+  }, [delay]);
 };
 
-//reducer.js
-export const reducer = (state, action) => {
-  switch (action.type) {
-    case 'CHANGE_TITLE': {
-      return { ...state, title: action.payload };
-    }
-  }
-
-  return { ...state };
-};
-
-//AppContext.jsx
-export const Context = createContext();
-export const AppContext = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, globalState);
-
-  const changeTitle = (payload) => {
-    dispatch({ type: 'CHANGE_TITLE', payload });
-  };
-
-  return <Context.Provider value={{ state, changeTitle }}>{children}</Context.Provider>;
-};
-
-AppContext.propTypes = {
-  children: P.node,
-};
-
-//Components/H1/index.jsx
-const H1 = () => {
-  const context = useContext(Context);
-  const inputRef = useRef();
-
-  return (
-    <>
-      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>{context.state.title}</h1>
-      <input ref={inputRef} type="text" />
-    </>
-  );
-};
-
-// App.jsx
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [delay, setDelay] = useState(1000);
+
+  useMyHook(() => setCounter((c) => c + 1), delay);
+
+  const handleClickInc = () => setDelay((d) => d + 1000);
+  const handleClickDec = () => setDelay((d) => d - 1000);
+
   return (
-    <AppContext>
-      <div className="App">
-        <H1 />
-      </div>
-    </AppContext>
+    <div className="App">
+      <h1>Contador: {counter}</h1>
+      <button onClick={handleClickInc}>Diminuir velocidade</button>
+      <button onClick={handleClickDec}>Aumentar velocidade</button>
+    </div>
   );
 }
 
