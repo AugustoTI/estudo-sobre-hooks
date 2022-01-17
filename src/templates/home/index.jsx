d1690196ed7bb1216221afd903f2310dd6ecebc4
AugustoTI <augustocesarti2021@gmail.com>
2022-01-17 00:59:28 -0300

    criando meu segundo Hook parte 2

--- src/templates/home/index.jsx
+++ src/templates/home/index.jsx
@@ -1,53 +1,97 @@
-import { useEffect, useState } from 'react';
-import './styles.css';
+import { useEffect, useRef, useState } from 'react';
+
+const isObjectEqual = (objA, objB) => {
+  return JSON.stringify(objA) === JSON.stringify(objB);
+};
 
 const useFecth = (url, options) => {
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [shouldLoad, setShouldLoad] = useState(false);
+  const urlRef = useRef(url);
+  const optionRef = useRef(options);
 
   useEffect(() => {
+    if (!isObjectEqual(urlRef.current, url)) {
+      urlRef.current = url;
+      setShouldLoad((prev) => !prev);
+    }
+
+    if (!isObjectEqual(optionRef.current, options)) {
+      optionRef.current = options;
+      setShouldLoad((prev) => !prev);
+    }
+  }, [url, options]);
+
+  useEffect(() => {
+    let wait = false;
     setLoading(true);
 
     const fecthPosts = async () => {
-      await new Promise((r) => setTimeout(r, 4000));
+      await new Promise((r) => setTimeout(r, 2000));
 
       try {
-        const postsRes = await fetch(url);
+        const postsRes = await fetch(urlRef.current, optionRef.current);
         const postsJSON = await postsRes.json();
-        setResult(postsJSON);
-        setLoading(false);
+        if (!wait) {
+          setResult(postsJSON);
+          setLoading(false);
+        }
       } catch (err) {
-        setLoading(false);
-        console.log(err);
+        if (!wait) {
+          setLoading(false);
+          console.log(err);
+        }
       }
     };
 
     fecthPosts();
-  }, [url, options]);
+
+    return () => {
+      wait = true;
+    };
+  }, [shouldLoad]);
 
   return [result, loading];
 };
 
 function Home() {
-  const [result, loading] = useFecth('https://jsonplaceholder.typicode.com/posts');
+  const [postID, setPostID] = useState('');
+  const [result, loading] = useFecth('https://jsonplaceholder.typicode.com/posts/' + postID, { abc: 123 });
 
-  if (!loading) {
-    console.log(result);
+  useEffect(() => {
+    console.log(postID);
+  }, [postID]);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
   }
 
-  return (
-    <>
-      {loading && <h1>Loading...</h1>}
+  const handleClick = (id) => {
+    setPostID(id);
+  };
 
-      {result &&
-        result.map((post) => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
+  if (!loading && result) {
+    return (
+      <div>
+        {result?.length > 0 ? (
+          result.map((post) => (
+            <div key={post.id}>
+              <h1 onClick={() => handleClick(post.id)}>{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          ))
+        ) : (
+          <div onClick={() => handleClick('')}>
+            <h1>{result.title}</h1>
+            <p>{result.body}</p>
           </div>
-        ))}
-    </>
-  );
+        )}
+      </div>
+    );
+  }
+
+  return <h1>Oi</h1>;
 }
 
 export default Home;
